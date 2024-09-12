fffe51db3d5fc44f5f9216f47885d640008e7245
fisheeesh <swanphyo444@gmail.com>
2024-09-12 21:49:56 +0700

    project setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <p>Hello {{ name }}</p>
+    <p>I am {{ age }} years old.</p>
+    <button @click="handleClick">Click</button>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'HomeView',
-  components: {
+  setup(){
+    // console.log('hi there')
+    let name = "Swan Yi Phyo"
+    let age = 22
+    // console.log(name)
+    let handleClick = () => console.log("clicked")
+    return{ name, age, handleClick }
   }
 }
 </script>
