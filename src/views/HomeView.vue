ad36674c46f6b2dfd1bf1c419ebbe058c93205d7
fisheeesh <swanphyo444@gmail.com>
2024-09-12 23:56:21 +0700

    computed property

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,48 +1,39 @@
 <template>
   <div class="home">
-    <!-- <h1>Person One</h1>
-    <p>{{ personOne.name }} - {{ personOne.age }}</p>
-    <button @click="changeOne">Change One</button>
-
-    <h1>Person Two</h1>
-    <p>{{ personTwo.name }} - {{ personTwo.age }}</p>
-    <button @click="changeTwo">Change Two</button> -->
-
-    <h1>User One - {{ userOne }}</h1>
-    <button @click="changeOne">Change One</button>
-    <h1>User Two - {{ userTwo }}</h1>
-    <button @click="changeTwo">Change Two</button>
+    <!-- <h1>{{ name }}</h1> -->
+    <input type="text" v-model="search">
+    <p>Search Item - {{ search }}</p>
+    <div v-for="name in filteredNames" :key="name">
+      {{ name }}
+    </div>
   </div>
 </template>
 
 <script>
-import { reactive, ref } from 'vue';
+import { computed, ref } from 'vue';
+
 export default {
   setup() {
-    // let personOne = ref({
-    //   name: 'John',
-    //   age: 25
+    // let name = computed(() => {
+    //   return "Swan Yi Phyo"
     // })
+    let search = ref('')
+    let names = ref([
+      'Swan Yi Phyo',
+      'Hla Hla',
+      'Tun Tun',
+      'Su Su',
+      'Maung Maung',
+      'Kyaw Kyaw'
+    ])
 
-    // let personTwo = reactive({
-    //   name: "Jane",
-    //   age: 26
-    // }) 
-    // // console.log(personTwo)
-    // // console.log(personOne)
-
-    // let changeOne = () => personOne.value.name = "Swan Yi Phyo"
-    // let changeTwo = () => personTwo.name = "Fisheeesh"
-
-    let userOne = ref("Swan Yi Phyo")
-    let userTwo = reactive("Fisheeesh")
-
-    let changeOne = () => userOne.value = "John"
-    //this will not work cuz reactive can not be worked with primitive values
-    let changeTwo = () => userTwo = "Jane"
+    //computed property has to return
+    let filteredNames = computed(()=>{
+      return names.value.filter(name => name.includes(search.value))
+    })
 
 
-    return { userOne, userTwo, changeOne, changeTwo }
+    return { names, search, filteredNames }
   }
 }
 </script>
