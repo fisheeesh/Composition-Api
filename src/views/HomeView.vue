100c6d0730f2c810f949381c74d925317d4fa050
fisheeesh <swanphyo444@gmail.com>
2024-09-12 22:05:56 +0700

    ref for reactivity

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,19 +3,31 @@
     <h1>Home</h1>
     <p>Hello {{ name }}</p>
     <p>I am {{ age }} years old.</p>
+
+    <input type="text" v-model="name">
     <button @click="handleClick">Click</button>
+    <button @click="age++">Increase Age</button>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 
 export default {
   setup(){
-    // console.log('hi there')
-    let name = "Swan Yi Phyo"
-    let age = 22
-    // console.log(name)
-    let handleClick = () => console.log("clicked")
+    /**
+     * no reactive code
+     */
+    // let name = "Swan Yi Phyo"
+
+    /**
+     * reactive code
+     */
+    let name  = ref("Swan Yi Phyo")
+    console.log(name.value)
+    let age = ref(22)
+    let handleClick = () => name.value = "Fisheeesh"
     return{ name, age, handleClick }
   }
 }
